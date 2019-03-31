9efc78fba0f348ac5855b9953a44a2c93fb16cb4
JirachaiHuad <jirachai_u@hotmail.com>
2019-03-31 23:33:35 +0700

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -1,33 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { Provider } from 'react-redux'
+import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+import 'react-dates/lib/css/_datepicker.css'
 
-const store = configureStore();
-
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpense = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpense);
-});
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 111000 }));
+const store = configureStore()
 
 const jxs = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
-);
+)
 
-ReactDOM.render(jxs, document.getElementById('app'));
+ReactDOM.render(jxs, document.getElementById('app'))
