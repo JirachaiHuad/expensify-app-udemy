0102488aa9ec1d8404a556dcd4bcfee1cde3160b
JirachaiHuad <jirachai_u@hotmail.com>
2019-04-02 10:46:14 +0700

    Setup webpack production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,51 @@
-const path = require('path'); // [node function]
+const path = require('path') // [node function]
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // absolute path = /Users/jirachai/Documents/Online Courses/Udemy-React/indecision-app/public
 
-module.exports = {  // [node function]
-  entry: './src/app.js', // relative path for entry point
-  output: {
-    path: path.join(__dirname, 'public'), // absolute path on the computer
-    filename: 'bundle.js'  // any name technically
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/, // regular expression
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
+module.exports = (env) => {  // [node function]
+  const isProduction = env === 'production'
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' })
+
+  console.log(env)
+
+  return {
+    entry: './src/app.js', // relative path for entry point
+    output: {
+      path: path.join(__dirname, 'public'), // absolute path on the computer
+      filename: 'bundle.js'  // any name technically
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/, // regular expression
+        exclude: /node_modules/
+      }, {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+              loader: 'css-loader',
+              options: {
+                  sourceMap: true
+              }
+          },
+          {
+              loader: 'sass-loader',
+              options: {
+                  sourceMap: true
+              }
+          }
+        ]
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
   }
-};
+}
